924a5b269b7e1669c67870a2a4cbb38b80e43d61
S4UR000N <backtrack.grey@gmail.com>
2024-05-05 22:48:10 +0200

    Add basic folder structure

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,43 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../Frontend')));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.send('404 Not Found');
+  // res.sendFile(path.resolve(__dirname, '../Frontend/dist', 'index.html'));
+});
+
+app.post("/api/upload", (req, res) => {
+  let formData = req.body;
+
+  if(true)
+  {
+      res.status(200).json(formData);
+  } else {
+      res.status(400).json(formData);
+  }  
+});
+
+app.post("/api/download", (req, res) => {
+  let formData = req.body;
+
+  if(true)
+  {
+      res.status(200).json(formData);
+  } else {
+      res.status(400).json(formData);
+  }  
+});
+
+app.post("*", (req, res) => {
+  res.status(404).json({ error: '404 Not Found' });
+});
 
 
 app.listen(PORT, () => {
---
+++ Backend/src/model/response/response.model.js
@@ -0,0 +1,5 @@
+class ResponseModel {
+    success = false;
+    errors = [];
+    result = {};
+}
