b0dda09ba5e71fddafe650424a8935df578ac7a9
zana <zderic.ana20@gmail.com>
2024-05-07 21:17:56 +0200

    Add txt to docx conversion

--- Backend/src/service/file.service.js
+++ Backend/src/service/file.service.js
@@ -1,9 +1,18 @@
 import FileRepository from '../repository/file.repository.js';
+import FileModel from '../model/entity/file.model.js';
+import mammoth from 'mammoth';
 
 class FileService {
     fileRepository = new FileRepository();
 
-    uploadFile(fileModel) {
+    async uploadFile(file) {
+        const options = { transformDocument: mammoth.transforms.docx()};
+
+        const docxData = await mammoth.convertText({ buffer: file.buffer }, options);
+        const binaryData = new Binary(docxData);
+        
+        let fileModel = new FileModel(file.name, binaryData);
+
         this.repository.createFile(fileModel);
     }
     searchFile(fileId) {
