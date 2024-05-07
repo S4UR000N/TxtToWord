8bce911be146d7f674c889437dc4ee63c34543a9
zana <zderic.ana20@gmail.com>
2024-05-07 23:01:23 +0200

    Update FileSchema, implement search and download file endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -7,11 +7,9 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 import FileService from './src/service/file.service.js';
-// let fileService = new FileService();
-// fileService.searchFile(1);
 
 const app = express();
-const upload = multer();
+const upload = multer({ limits: {fileSize: 5 * 1024 * 1024 }});
 
 const PORT = 3000;
 
@@ -21,40 +19,47 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
 app.get('/api/search/:fileId', (req, res) => {
-  let resData = { fileId: req.params.fileId, fileExists: true };
-  res.status(200).json(resData);
+    let data = req.params.fileId;
+    try {
+        const file = FileService.searchFile(data);
+        if(!file) {
+            return res.status(404).json({ error: 'File not found' });
+        }
+        res.send(file.name);
+    } catch(error) {
+        console.log('Error on search a file:', error);
+        res.status(500).json({ error: 'Error searching a file'});
+    }
 });
 
 app.get('/api/download/:fileId', (req, res) => {
-  let data = req.params.fileId;
-
-  if(true) {
-    res.status(200).json(formData);
-  } else {
-    res.status(400).json(formData);
-  }
-
-  // res.set({
-  //     'Content-Type': 'application/msword', // MIME type for Word file
-  //     'Content-Disposition': `attachment; filename=${file.name}` // Specify filename for download
-  // });
-  // res.send(file.data);
+    let data = req.params.fileId;
+    try {
+        const file = FileService.searchFile(data);
+        if(!file) {
+            return res.status(404).json({ error: 'File not found'});
+        }
+        res.setHeader('Content-Disposition', `attachment; filename=${file.name}`);
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-office-document.wordprocessingml.document');
+        res.send(file.bytes);
+    } catch(error) {
+        console.error('Error downloading a file:', error);
+        res.status(500).json({ error: 'Error downloading a file'});
+    }
 });
 
 app.post('/api/upload', upload.single('file'), async (req, res) => {
-  let file = req.file;
-
-  if(!file) {
-    return res.status(400).json({ error: 'No file uploaded' });
-  } 
-
-  try {
-    await FileService.uploadFile(file);
-    res.json({ message: 'File uploaded and converted to .docx' });
-  } catch(error) {
-    console.error('Error processing file:', error);
-    res.status(500).json({ error: 'Error processing a file' });
-  }
+    let file = req.file;
+    if(!file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+    } 
+    try {
+        await FileService.uploadFile(file);
+        res.json({ message: 'File uploaded and converted to .docx' });
+    } catch(error) {
+        console.error('Error processing file:', error);
+        res.status(500).json({ error: 'Error processing a file' });
+    }
 });
 
 app.get('*', (req, res) => {
