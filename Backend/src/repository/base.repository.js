085b9ccc61287cb0498d0608e166873d59f95afa
S4UR000N <backtrack.grey@gmail.com>
2024-05-11 18:46:51 +0200

    Fix logger service, update front to work with imporved back-end, cover more error cases, modify docker compose to be compatible with logger service (pino package)

--- Backend/src/repository/base.repository.js
+++ Backend/src/repository/base.repository.js
@@ -10,7 +10,6 @@ class BaseRepository {
             mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`)
             .then(_ => {
                 BaseRepository.hasConnected = true;
-                logger.info('db connection success');
             })
             .catch(err => {
                 logger.error('db connection failed\n' + err);
